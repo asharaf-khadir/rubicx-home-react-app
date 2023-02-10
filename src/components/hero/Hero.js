bff505e51b35f6dc3bfdb65325ab3bc69d4649d2
Asharaf khadir <asharafkhadir8@gmail.com>
2023-02-10 15:11:07 +0530

    Hero image and text added

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,14 @@
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <h1 className="title">Tech</h1>
+      <h4>FOR</h4>
+      <h1>TEENS</h1>
+      <h6>CARVING FUTURE</h6>
+      <h6>TECHNOLOGY PROFESSIONALS</h6>
+      <h6>OUT OF YOUNG MING</h6>
+    </div>
+  );
+}
