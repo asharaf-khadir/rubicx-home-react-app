e9f6a76321bb22d35eb3cc0b8246823156dfcff2
Asharaf khadir <asharafkhadir8@gmail.com>
2023-02-11 03:39:45 +0530

    form email and mobile number validation added

--- src/components/form/UserData.js
+++ src/components/form/UserData.js
@@ -1,6 +1,12 @@
 import "./UserData.css";
+import React, { useState } from "react";
 
 export default function UserData() {
+  const [userName, setUserName] = React.useState("");
+  const UsernameHandler = (event) => {
+    setUserName(event.target.value);
+  };
+  console.log(userName);
   return (
     <div className="form-section">
       <div className="form-heading">
@@ -15,27 +21,43 @@ export default function UserData() {
       </div>
 
       <form className="form-userdetails">
-        <label for="fullname">Name*</label>
-        <input
-          type="text"
-          id="fullname"
-          name="fullname"
-          placeholder="Enter Your Name"
-        ></input>
-        <label for="email">Email*</label>
-        <input
-          type="text"
-          id="email"
-          name="firstname"
-          placeholder="Enter Your Email"
-        ></input>
-        <label for="mobilenumber">Mobile Number*</label>
-        <input
-          type="text"
-          id="mobilenumber"
-          name="mobilenumber"
-          placeholder="Enter Your Mobile Number"
-        ></input>
+        <label>
+          Name*
+          <input
+            type="text"
+            name="fullname"
+            placeholder="Enter Your Name"
+            onChange={UsernameHandler}
+            maxlength="32"
+            pattern="[A-Za-z]{4}"
+            required
+          ></input>
+        </label>
+
+        <label>
+          Email*
+          <input
+            type="email"
+            name="fullname"
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+            title="Enter valid email : example@example.com"
+            placeholder="Enter Your Email"
+            required
+          ></input>
+        </label>
+
+        <label>
+          Mobile Number*
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            placeholder="Enter Your Mobile Number"
+            pattern="[0-9]{10}"
+            title="Enter valid 10 digit mobile number"
+            required
+          ></input>
+        </label>
 
         <button type="submit" className="form-submitbutton">
           Register Now
